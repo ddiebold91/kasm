60f10906fc9f2b7a3bce99eef52cae6f856646c1
ddiebold91 <86532014+ddiebold91@users.noreply.github.com>
2023-05-01 16:27:07 -0400

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    name: 'dhnhome2.online',
+    description: 'The kasm repository for dhnhome2.online.',
+    icon: '/site/thumbs_up.png',
+    listUrl: 'https://ddiebold91.github.io/kasm/',
+    contactUrl: 'https://ddiebold91.github.io/kasm/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kasm/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
